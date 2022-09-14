98e79fc926a468d01f89a508d3c420ab227639da
unknown <nazih.developer@gmail.com>
2022-09-14 21:34:58 +0200

    Completed mock version of CRUD functions connected to mongoose for food

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const foods = require("./routes/foods");
 const categories = require("./routes/categories");
 const logger = require("./middleware/logger");
+const mongoose = require("mongoose");
+const { number } = require("joi");
+const { appendFile } = require("fs");
+const { get } = require("http");
 
 const app = express();
 
@@ -9,6 +13,53 @@ app.use(express.json());
 app.use(logger);
 app.use("/api/foods", foods);
 app.use("/api/categories", categories);
+mongoose
+  .connect("mongodb://localhost/myown-intensive-foods")
+  .then(() => console.log("connect to MongoDB"))
+  .catch((error) => console.log("could not conecct...", error));
+
+const foodSchema = mongoose.Schema({
+  name: String,
+  category: String,
+  numberInStock: Number,
+  price: Number,
+});
+
+const Food = mongoose.model("food", foodSchema);
+
+async function createFood(food) {
+  const newFood = new Food(food);
+
+  try {
+    await newFood.save();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getFoods() {
+  const foods = await Food.find();
+  console.log(foods);
+}
+
+async function getOneFood(id) {
+  const foods = await Food.findById(id);
+  console.log(foods);
+}
+
+async function updateFood(id) {
+  await Food.findByIdAndUpdate(id, {
+    name: "banan",
+    category: "Fruit",
+    numberInStock: 3,
+    price: 5,
+  });
+}
+
+async function deleteOneFood(id) {
+  await Food.findByIdAndDelete(id);
+  console.log(foods);
+}
 
 app.listen(8000, () => {
   console.log("listening on port 8000...");
