fe889f88f325cfef42cd7edc926347fcfab45dff
unknown <nazih.developer@gmail.com>
2022-09-17 21:01:58 +0200

    Implemented post for Users

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,17 @@ const express = require("express");
 const { validate, User } = require("../models/User");
 const router = express.Router();
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.message);
+
+  const existingUser = await User.findOne({ email: req.body.email });
+  if (existingUser) return res.status(400).send("User already registered");
+
+  const user = new User(req.body);
+  user.save();
+
+  return res.status(201).send(user);
 });
 
 module.exports = router;
