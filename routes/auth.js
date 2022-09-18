ddd6ea60ca3c9291b4917c1218aac351acedb82e
unknown <nazih.developer@gmail.com>
2022-09-18 13:27:21 +0200

    Implemented joi validation for auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,23 @@
 const express = require("express");
+const { Joi } = require("joi");
+const { User } = require("../models/User");
 const router = express.Router();
 
-router.post("/", (req, ress) => {});
+router.post("/", async (req, ress) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.message);
+
+  const existingUser = await User.findOne({ email: req.body.email });
+  if (!existingUser) return res.status(400).send("Invalid email or Password");
+});
+
+function validate(user) {
+  const schema = Joi.object({
+    email: Joi.string.email().required(),
+    password: Joi.string.min(5).required(),
+  });
+
+  return schema.validte(user);
+}
 
 modeule.exports = router;
