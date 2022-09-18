f2deaeeb903eaf2a2660f38f573a9db25217b066
unknown <nazih.developer@gmail.com>
2022-09-18 15:27:54 +0200

    Start building the middleware auth

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ dotenv.config();
 
 app.use(cors());
 app.use(express.json());
-app.use(logger);
 app.use("/api/foods", foods);
 app.use("/api/users", users);
 app.use("/api/auth", auth);
--- middleware/logger.js
+++
@@ -1,6 +0,0 @@
-const logger = (req, res, next) => {
-  console.log("my first middleware!");
-  next();
-};
-
-module.exports = logger;
