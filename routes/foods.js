3111f28985d57f3d146b1e5b6a36907389132ebb
unknown <nazih.developer@gmail.com>
2022-09-18 15:45:29 +0200

    Extra completions on router /me on middleware get

--- routes/foods.js
+++ routes/foods.js
@@ -7,7 +7,7 @@ const User = require("../models/User");
 const router = express.Router();
 
 router.get("/me", auth, async (req, res) => {
-  const user = await User.findById(req.user._id);
+  const user = await User.findById(req.user._id).select("-password");
   if (!user) return res.status(404).send("User not found");
 
   return res.send(user);
